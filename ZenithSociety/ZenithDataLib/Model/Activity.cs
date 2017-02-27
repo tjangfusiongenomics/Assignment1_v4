a000e09e730f743566bf404b9ede25dc5a163c76
tjang <tjang@fusiongenomics.com>
2017-02-26 17:36:55 -0800

    first commit

---
+++ ZenithSociety/ZenithDataLib/Model/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithDataLib.Model
+{
+    public class Activity
+    {
+        [Key]
+        public int ActivityId { get; set; }
+        [Display(Name = "Activity")]
+        public string ActivityDescription { get; set; }
+        public DateTime CreationData { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+    }
+}
